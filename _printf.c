12f1ea168b248defda3a1b58da5fc476771e5539
laytoshcode <laytoshtwo@yahoo.com>
2022-12-28 04:31:02 -0800

    Printf project

--- _printf.c
+++ _printf.c
@@ -57,19 +57,10 @@ int _printf(const char *format, ...)
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
-void print_buffer(char buffer[], int *buff_ind);
+void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
 }
-
-
-~
-~
-~
-~
-~
-~
-
